baa85c04801606e6c058d1a68cb7744b9a864d01
dwu <brycenycuu@gmail.com>
2019-03-01 15:57:15 -0500

    First commit

---
+++ middleware/index.js
@@ -0,0 +1,61 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// Add all middlewares
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                req.flash("error", "Campground not found.");
+                res.redirect("back")
+            } else {
+                // does he own the campground?
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                    // res.render("campgrounds/edit", {campground: foundCampground, currentUser: req.user});
+                } else {
+                    req.flash("error", "You don't have permission to do that.");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "You need to login first to do that.");
+        res.redirect("back");
+    }
+};
+
+
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back")
+            } else {
+                // does he own the comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                    // res.render("campgrounds/edit", {campground: foundCampground, currentUser: req.user});
+                } else {
+                    req.flash("error", "You don't have permission to do that.");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "You need to login first to do that.");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    };
+    req.flash("error", "You need to login first to do that.");
+    res.redirect("/login");
+};
+
+module.exports = middlewareObj;
